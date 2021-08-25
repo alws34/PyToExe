47347e637b7eefefac7e5675588aa18f7f113b9d
alws34 <alws34@gmail.com>
2021-08-26 02:04:10 +0300

    init

---
+++ frmPytoExe.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace PythonToExe
+{
+    partial class frmPyToExe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(225, 79);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(150, 56);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(119, 29);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(419, 22);
+            this.textBoxPath.TabIndex = 1;
+            this.textBoxPath.TextChanged += new System.EventHandler(this.textBoxPath_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File FULL path";
+            // 
+            // frmPyToExe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 160);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.btnRun);
+            this.Name = "frmPyToExe";
+            this.Text = "Py to exe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmPytoExe.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace PythonToExe
+{
+    public partial class frmPyToExe : Form
+    {
+        public frmPyToExe()
+        {
+            InitializeComponent();
+        }
+
+        private void compile()
+        {
+            try
+            {
+                string full_file_path = textBoxPath.Text;
+
+                if (!String.IsNullOrWhiteSpace(full_file_path))
+                {
+                    if (full_file_path.Contains("\""))//remove parentheses
+                        full_file_path = full_file_path.Replace("\"", "");
+
+                    string filename_WO_Ext = Path.GetFileNameWithoutExtension(full_file_path);//get file name wihtout extension
+                    string desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);//get current user desktop folder
+                    string work_folder = desktop + @"\" + filename_WO_Ext; // put the new folder on the desktop
+                    string bat_file = work_folder + @"\Pycompile.bat"; //bat file path
+
+                    check_dir(work_folder);
+                    check_file(work_folder);
+                    
+                    using (StreamWriter sw = new StreamWriter(bat_file))//write commands to file
+                    {
+                        sw.WriteLine("cd /d " + "\"" + work_folder); // cd into work folder 
+                        sw.WriteLine("pyinstaller --onefile " + "\"" + full_file_path + "\""); // run the python compiler
+                        sw.WriteLine("explorer " + work_folder + @"\dist"); //open the folder where the exe is
+                    }
+
+                    Process.Start(bat_file);//run batch file
+                }
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show(ioe.ToString());
+            }
+        }
+
+        private void check_dir(string path)//check if directory exists
+        {
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
+
+        private void check_file(string path) //check if path exists
+        {
+            if (!File.Exists(path))
+                File.Create(path);
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            compile();
+        }
+
+        private void textBoxPath_TextChanged(object sender, EventArgs e)
+        {
+            string path = textBoxPath.Text;//validate the file path (the file) exists
+            path = path.Replace("\"", "");
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("file does not exists or path is invalid");
+                textBoxPath.Text = "";
+            }
+        }
+    }
+}
